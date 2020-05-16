04f7782697109fa60cd5d599b864f1ca3ea9530c
mfritzen <mfritzen@purdue.edu>
2020-05-16 16:35:15 -0400

    program functional

--- mazeSolver.py
+++ mazeSolver.py
@@ -1,3 +1,5 @@
+done = False
+
 def getMaze(filename):
 	
 	maze = []
@@ -26,6 +28,107 @@ def getCoord(maze, target):
 
 	return (i, j)
 
+def makeMazeList(maze):
+	maze_as_list = []
+	for line in maze:
+		line_as_list = []
+		for block in line:
+			line_as_list.append(block)
+		maze_as_list.append(line_as_list)
+	return maze_as_list
+
+def solveMaze(maze, start, end):
+	x = start[1]
+	y = start[0]
+
+	# go north
+	maze = solveRecurse(maze, (y-1, x), 0)
+	# go east
+	maze = solveRecurse(maze, (y, x+1), 1)
+	# go south
+	maze = solveRecurse(maze, (y+1, x), 2)
+	# go west
+	maze = solveRecurse(maze, (y, x-1), 3)
+
+	return maze
+
+def solveRecurse(maze, coord, direction):
+	y = coord[0]
+	x = coord[1]
+	path_char = "o"
+
+	if ((y < 0) or (y >= len(maze)) or (x < 0) or (x >= len(maze[0]))):
+		return maze
+	if maze[y][x] == "X":
+		return maze
+	if maze[y][x] == "U":
+		return maze
+	
+	if maze[y][x] == "E":
+		maze[y][x] = path_char
+		global done
+		done = True
+		return maze
+
+	if maze[y][x] == " ":
+		maze[y][x] = "U"
+		if direction == 0: # previous move was north
+			maze = solveRecurse(maze, (y-1, x), 0) # go north
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y, x+1), 1) # go east
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y, x-1), 3) # go west
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+		if direction == 1: # previous move was east
+			maze = solveRecurse(maze, (y-1, x), 0) # go north
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y, x+1), 1) # go east
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y+1, x), 2) # go south
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+		if direction == 2: # previous move was south
+			maze = solveRecurse(maze, (y, x+1), 1) # go east
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y+1, x), 2) # go south
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y, x-1), 3) # go west
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+		if direction == 3: # previous move was west
+			maze = solveRecurse(maze, (y-1, x), 0) # go north
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y+1, x), 2) # go south
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+			maze = solveRecurse(maze, (y, x-1), 3) # go west
+			if done == True:
+				maze[y][x] = path_char
+				return maze
+		
+	if done == True:
+		maze[y][x] = path_char
+	return maze
+
 def removeJunk(maze, junk):
 	clean_maze = []
 	for line in maze:
@@ -49,6 +152,7 @@ if __name__ == "__main__":
 	maze = getMaze("maze.txt")
 	end_point = getCoord(maze, "E")
 	start_point = getCoord(maze, "S")
-
+	maze = makeMazeList(maze)
+	maze = solveMaze(maze, start_point, end_point)
 	maze = removeJunk(maze, "U")
 	writeSol("maze_solution.txt", maze)
